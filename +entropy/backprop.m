e98c7727d91f93b5bf966da7f9c91ede499e05b6
Olaf <olaf.schab@gmail.com>
2017-06-23 20:00:24 +0200

    net configuration

--- +entropy/backprop.m
+++
@@ -1,6 +0,0 @@
-function dn = backprop(da,n,a,param)
-%TANSIG.BACKPROP
-
-% Copyright 2012 The MathWorks, Inc.
-
-dn = bsxfun(@times,da,1-(a.*a));
--- +entropy/forwardprop.m
+++
@@ -1,6 +0,0 @@
-function da = forwardprop(dn,n,a,param)
-%TANSIG.FORWARDPROP
-
-% Copyright 2012 The MathWorks, Inc.
-
-da = bsxfun(@times,dn,1-(a.*a));

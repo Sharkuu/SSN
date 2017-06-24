890402333bccb7d8adf1a1a12fdea5377f5e7e58
Olaf <olaf.schab@gmail.com>
2017-06-24 09:50:30 +0200

    only one hidden layer

--- iris.m
+++ iris.m
@@ -1,7 +1,7 @@
 clear all;
 load iris_dataset;
 ilosc_podzialow = 10;
-ilosc_neuronow = [6 10 15];
+ilosc_neuronow = [6];
 ilosc_epok = 100;
 %mieszanie danych 
 
@@ -36,12 +36,14 @@ wyjscie_uczace = podzial{2,1};
 net = configure(net, wejscie_uczace, wyjscie_uczace);
 net.IW{1,1} = rand(ilosc_neuronow(1),size(wejscie_uczace,1));
 net.b{1} =rand(ilosc_neuronow(1),1);
-for i=1:size(ilosc_neuronow,2)-1
-    net.LW{i+1,i} = rand(ilosc_neuronow(i+1),ilosc_neuronow(i))
-    net.b{i+1} = rand(ilosc_neuronow(i+1),1);
-end
-net.LW{size(wejscie_uczace,1),size(ilosc_neuronow,2)} = rand(size(wyjscie_uczace,1),ilosc_neuronow(size(ilosc_neuronow,2)));        
-net.b{size(ilosc_neuronow,2)+1} = rand(size(wyjscie_uczace,1),1);   
+net.LW{2} =rand(size(wyjscie_uczace,1),ilosc_neuronow(1));
+net.b{2} = rand(size(wyjscie_uczace,1),1);
+% for i=1:size(ilosc_neuronow,2)-1
+%     net.LW{i+1,i} = rand(ilosc_neuronow(i+1),ilosc_neuronow(i))
+%     net.b{i+1} = rand(ilosc_neuronow(i+1),1);
+% end
+% net.LW{size(wejscie_uczace,1),size(ilosc_neuronow,2)} = rand(size(wyjscie_uczace,1),ilosc_neuronow(size(ilosc_neuronow,2)));        
+% net.b{size(ilosc_neuronow,2)+1} = rand(size(wyjscie_uczace,1),1);   
 %%%%%niby sie wszystko zawsze wypelnia randomowo, ale zrobilem to jeszcze
 %%%%%raz
 

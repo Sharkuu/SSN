1431b31c612ae3184b0bf4159add80d943d15f40
Olaf <olaf.schab@gmail.com>
2017-06-24 10:55:35 +0200

    output for each neuron (whole learning set)

--- iris.m
+++ iris.m
@@ -87,30 +87,8 @@ h = 1;
     
     
 % % % % % % % % % % % % % % % %   WARTOŒCI 
-input_layer = [];
-bias = net.b{1};
-input = net.IW{1};
-k = 1 ;%to bedzie dla kazdego
-% % mam nadzieje ze dobrze to zrobilem XDD
-for i=1:ilosc_neuronow
-    b = bias(i,1);
-    tmp = 0;
-    for j=1:size(wejscie_uczace,1)
-        tmp = tmp + wejscie_uczace(j,k)*input(i,j);
-    end
-    input_layer(i) = b+tmp;
-end
-bias = net.b{2};
-input = net.LW{2};
-output_layer = [];
-for i=1:size(wyjscie_uczace,1)
-    b = bias(i,1);
-    tmp = 0;
-    for j=1:ilosc_neuronow
-        tmp = tmp + input_layer(j)*input(i,j);
-    end
-    output_layer(i) = b + tmp;
-end
+wyjscia_neuronow = oblicz_wyjscia_neuronow(net, wejscie_uczace, wyjscie_uczace,ilosc_neuronow);
+
         
 
 

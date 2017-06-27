98e904006baa076f023aa9035ed81d2a792a029b
Olaf <olaf.schab@gmail.com>
2017-06-27 13:10:18 +0200

    .

--- back_prop_input.m
+++ back_prop_input.m
@@ -1,8 +1,7 @@
 function [ delta ] = back_prop_input(waga,waga_out,neuron_number,n,numer_cechy,delta,wejscie)
 
-waga_out = waga_out(neuron_number);
 d = delta(neuron_number, n);
-waga = waga(neuron_number,numer_cechy);
+
 v = wejscie(numer_cechy,n);
 u = v*waga;
 % delta = d*waga_out*d_tansig(u)*v;
